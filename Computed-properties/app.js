61b7565f15c7ab114ecade5228da51eb7b67b6f6
Carlos Gerardo Perez Horta <carlos.perez@charliegperez.com>
2021-01-17 02:27:50 -0600

    Manipulación del DOM

---
+++ Computed-properties/app.js
@@ -0,0 +1,46 @@
+new Vue({
+  el: '#app',
+
+  data() {
+    return {
+      name: 'Bitcoin',
+      img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+      changePercent: 1,
+      price: 8400,
+      prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+      info: [123, 456, 789, 101112, 131415, 161718, 192021],
+      pricesWithDays: [
+        { day: 'Lunes', value: 8400 },
+        { day: 'Martes', value: 7900 },
+        { day: 'Miercoles', value: 8200 },
+        { day: 'Jueves', value: 9000 },
+        { day: 'Viernes', value: 9400 },
+        { day: 'Sabado', value: 10000 },
+        { day: 'Domingo', value: 10200 },
+      ],
+      showPrices: false,
+      color: 'a1c3e6',
+      symbol: 'BTC',
+    };
+  },
+
+  computed: {
+    title() {
+      return `${this.name} - ${this.symbol}`;
+    },
+  },
+
+  watch: {
+    showPrices(newVal, oldVal) {
+      console.log(newVal, oldVal);
+    },
+  },
+
+  methods: {
+    toogleShowPrices() {
+      this.showPrices = !this.showPrices;
+
+      this.color = this.color.split('').reverse().join('');
+    },
+  },
+});
