0a331884a1446fba3074e2446a75a5d7a3f1e81b
Carlos Gerardo Perez Horta <carlos.perez@charliegperez.com>
2021-01-16 23:50:08 -0600

    first commit

---
+++ Eventos/app.js
@@ -0,0 +1,29 @@
+new Vue({
+  el: '#app',
+
+  data() {
+    return {
+      name: 'Bitcoin',
+      img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+      changePercent: 0,
+      prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+      info: [123, 456, 789, 101112, 131415, 161718, 192021],
+      pricesWithDays: [
+        { day: 'Lunes', value: 8400 },
+        { day: 'Martes', value: 7900 },
+        { day: 'Miercoles', value: 8200 },
+        { day: 'Jueves', value: 9000 },
+        { day: 'Viernes', value: 9400 },
+        { day: 'Sabado', value: 10000 },
+        { day: 'Domingo', value: 10200 },
+      ],
+      showPrices: false,
+    };
+  },
+
+  methods: {
+    toogleShowPrices() {
+      this.showPrices = !this.showPrices;
+    },
+  },
+});
